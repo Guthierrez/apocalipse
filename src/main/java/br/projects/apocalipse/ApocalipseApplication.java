c80e0e5c362bd865e7eb6789be86692dc297cfed
Guthierrez <guthi.note@outlook.com>
2017-05-17 08:55:51 -0300

    Commit Inicial projeto em branco

---
+++ src/main/java/br/projects/apocalipse/ApocalipseApplication.java
@@ -0,0 +1,12 @@
+package br.projects.apocalipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApocalipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApocalipseApplication.class, args);
+	}
+}
