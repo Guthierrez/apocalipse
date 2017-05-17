0c8ec2d4295d721499047796f9bbb65392d276d3
Guthierrez <guthi.note@outlook.com>
2017-05-17 11:22:58 -0300

    método para remover item usuário

--- src/main/java/br/projects/apocalipse/service/UsuarioService.java
+++ src/main/java/br/projects/apocalipse/service/UsuarioService.java
@@ -12,48 +12,58 @@ import br.projects.apocalipse.repository.Usuarios;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private Usuarios usuarios;
-	
-	public List<Usuario> listarUsuarios(){
+
+	public List<Usuario> listarUsuarios() {
 		return usuarios.findAll();
 	}
-	
+
 	public Usuario salvarUsuario(Usuario usuario) {
 		return this.usuarios.save(usuario);
 	}
-	
-	public void removerUsuario(Long id){
-		if(usuarios.findOne(id) != null){
+
+	public void removerUsuario(Long id) {
+		if (usuarios.findOne(id) != null) {
 			this.usuarios.delete(id);
 		}
 	}
-	
-	public Usuario atualizarLocalizacaoUsuario(Long id, Localizacao localizacao){
+
+	public Usuario atualizarLocalizacaoUsuario(Long id, Localizacao localizacao) {
 		Usuario usuario = this.usuarios.findOne(id);
-		if(usuario != null){
+		if (usuario != null) {
 			usuario.setUltima(localizacao);
 			return this.usuarios.save(usuario);
-		}else{
+		} else {
 			return null;
 		}
-			
+
 	}
 
 	public Object adicionarItemUsuario(Long id, Item item) {
 		Usuario usuario = this.usuarios.findByIdAndFetchEager(id);
-		if(usuario != null){
+		if (usuario != null) {
 			usuario.getItens().add(item);
 			return this.usuarios.save(usuario);
-		}else{
+		} else {
 			return null;
-		}	
+		}
+	}
+
+	public Object removerItemUsuario(Long id, Item item) {
+		Usuario usuario = this.usuarios.findByIdAndFetchEager(id);
+		if (usuario != null) {
+			if (usuario.getItens().contains(item)) {
+				usuario.getItens().remove(item);
+				return this.usuarios.save(usuario);
+			}
+		}
+		return null;
 	}
 
 	public Usuarios getUsuarios() {
 		return usuarios;
 	}
-
 
 }
